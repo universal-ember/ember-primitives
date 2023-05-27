f0874af12346f73312bfb2fba8eb1309ae724ed6
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2023-05-26 21:38:05 -0400

    ember-try adjustments

--- test-app/config/ember-try.js
+++ test-app/config/ember-try.js
@@ -28,6 +28,14 @@ module.exports = async function () {
         },
       },
       {
+        name: 'ember-5.0',
+        npm: {
+          devDependencies: {
+            'ember-source': '~5.0.0',
+          },
+        },
+      },
+      {
         name: 'ember-release',
         npm: {
           devDependencies: {
@@ -48,24 +56,6 @@ module.exports = async function () {
         npm: {
           devDependencies: {
             'ember-source': await getChannelURL('canary'),
-          },
-        },
-      },
-      {
-        name: 'ember-classic',
-        env: {
-          EMBER_OPTIONAL_FEATURES: JSON.stringify({
-            'application-template-wrapper': true,
-            'default-async-observers': false,
-            'template-only-glimmer-components': false,
-          }),
-        },
-        npm: {
-          devDependencies: {
-            'ember-source': '~3.28.0',
-          },
-          ember: {
-            edition: 'classic',
           },
         },
       },

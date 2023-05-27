048be44c3435e48c884f13d2b6be574d255baaae
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2023-05-26 23:36:36 -0400

    Update todo and CI

--- dev/bin/todo.js
+++ dev/bin/todo.js
@@ -1,57 +1,21 @@
 import { execa } from 'execa';
 
-/**
- * I hope one day turbo implements --dry-run --json and I can delete all this
- */
-
 async function pending(command) {
-  let { stdout } = await execa('pnpm', ['turbo', 'run', command, '--dry-run']);
+  let { stdout } = await execa('pnpm', ['turbo', 'run', command, '--dry-run=json']);
 
-  let [, info] = stdout.split('Tasks to Run');
-  let lines = info.split('\n').filter(Boolean);
-
-  let groups = [];
-  let currentGroup = [];
-
-  for (let line of lines) {
-    if (!line.match(/^\s/)) {
-      if (currentGroup) {
-        groups.push(currentGroup);
-      }
-
-      currentGroup = [line];
-
-      continue;
-    }
-
-    currentGroup.push(line);
-  }
-
-  groups.push(currentGroup);
-
-  groups = groups.filter((group) => group.length);
-  groups = groups.map((group) => group.map((line) => line.trim()));
+  let json = JSON.parse(stdout);
+  let tasks = json.tasks;
 
   let result = {};
 
-  // change to project => cached status
-  for (let group of groups) {
-    let [title, ...lines] = group;
-
-    if (!title.includes(command)) {
-      continue;
+  for (let task of tasks) {
+    if (task.task === command) {
+      result[task.package] = task.cache.status === 'MISS';
     }
-
-    let [projectName] = title.split('#');
-    let cacheLine = lines.find((line) => line.includes('Cached (Remote)'));
-    let localCacheLine = lines.find((line) => line.includes('Cached (Local)'));
-    let isCached =
-      cacheLine.includes('true') || localCacheLine.includes('true');
-
-    result[projectName] = isCached;
   }
 
   return result;
+
 }
 
 /**

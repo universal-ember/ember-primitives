cc961823006e33f9b379c688387ce0e6a8886d2f
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2023-06-03 01:03:30 -0400

    Global a11y setup is not strict-mode compatible

--- docs-app/tests/application/pages-test.ts
+++ docs-app/tests/application/pages-test.ts
@@ -1,20 +1,60 @@
-import { findAll, visit } from '@ember/test-helpers';
+import { findAll, settled, visit, waitUntil } from '@ember/test-helpers';
 import { module, test } from 'qunit';
 import { setupApplicationTest } from 'ember-qunit';
+
+import { colorScheme } from 'ember-primitives/color-scheme';
+
+import { a11yAudit } from 'ember-a11y-testing/test-support';
+
+/**
+ * a11yAudit halts tests, this gets around that
+ */
+async function checkA11y(assert: Assert, path: string, theme: string) {
+  await settled();
+
+  try {
+    await a11yAudit();
+    assert.ok(
+      true,
+      `no a11y errors found for ${path} using the ${theme} theme`
+    );
+  } catch (e) {
+    assert.ok(
+      false,
+      `no a11y errors found for ${path} using the ${theme} theme` +
+        `\n\n` +
+        e.message
+    );
+  }
+}
 
 module('Application | Pages', function (hooks) {
   setupApplicationTest(hooks);
 
   test('Visit each page in the nav', async function (assert) {
     await visit('/');
+    await waitUntil(() => findAll('nav a').length !== 0);
 
     let links = findAll('nav a');
 
-    assert.expect(links.length);
+    // the a11y page deliberately has violations on it
+    links = links.filter((link) => !link.href.includes('accessibility'));
+
+    assert.expect(links.length * 3);
 
     for (let link of links) {
-      await visit(link.href);
-      assert.ok(true, `no a11y errors found for ${link.href}`);
+      if (!('href' in link)) continue;
+
+      let url = new URL(link.href);
+
+      await visit(url.pathname);
+      await checkA11y(assert, link.href, 'default');
+
+      colorScheme.update('dark');
+      await checkA11y(assert, link.href, 'dark');
+
+      colorScheme.update('light');
+      await checkA11y(assert, link.href, 'light');
     }
   });
 });
--- docs-app/tests/test-helper.ts
+++ docs-app/tests/test-helper.ts
@@ -2,20 +2,12 @@ import { setApplication } from '@ember/test-helpers';
 import * as QUnit from 'qunit';
 import { setup } from 'qunit-dom';
 import { start } from 'ember-qunit';
-import {
-  setupGlobalA11yHooks,
-  DEFAULT_A11Y_TEST_HELPER_NAMES,
-} from 'ember-a11y-testing';
 
 import Application from 'docs-app/app';
 import config from 'docs-app/config/environment';
 
 setApplication(Application.create(config.APP));
 
-setupGlobalA11yHooks(() => true, {
-  // visit, click, + render tab, etc
-  helpers: [...DEFAULT_A11Y_TEST_HELPER_NAMES, 'render', 'tab'],
-});
 setup(QUnit.assert);
 
 start();

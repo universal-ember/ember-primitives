0a91d2c70c9f12202ed78e96b5fa960e04bda143
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2023-10-22 22:53:25 -0400

    Add sticky footer

--- ember-primitives/src/index.ts
+++ ember-primitives/src/index.ts
@@ -8,6 +8,7 @@ export { Avatar } from './components/avatar';
 export { Dialog, Dialog as Modal } from './components/dialog';
 export { ExternalLink } from './components/external-link';
 export { Form } from './components/form';
+export { StickyFooter } from './components/layout/sticky-footer/index';
 export { Link } from './components/link';
 export { OTP, OTPInput } from './components/one-time-password/index';
 export { Popover } from './components/popover';

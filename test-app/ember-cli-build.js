c3a10baa834d212347cdc81940a92ee8f24052b6
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2023-12-13 15:26:14 -0500

    shorthand

--- test-app/ember-cli-build.js
+++ test-app/ember-cli-build.js
@@ -14,15 +14,11 @@ module.exports = function (defaults) {
 
   const { Webpack } = require('@embroider/webpack');
 
-  return require('@embroider/compat').compatBuild(app, Webpack, {
-    extraPublicTrees: [],
-    staticAddonTrees: true,
-    staticAddonTestSupportTrees: true,
-    staticHelpers: true,
-    staticModifiers: true,
-    staticComponents: true,
-    staticEmberSource: true,
-    implicitModulesStrategy: 'packageNames',
+  const { compatBuild, recommendedOptions } = require('@embroider/compat');
+
+  return compatBuild(app, Webpack, {
+    ...recommendedOptions.optimized,
+    skipBabel: [{ package: 'qunit' }],
     packagerOptions: {
       webpackConfig: {
         devtool: 'source-map',

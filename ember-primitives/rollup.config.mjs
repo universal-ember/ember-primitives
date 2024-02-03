f18518d475eb4f1898a73c1c079839c5ce038225
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2024-02-03 18:14:57 -0500

    Update declaration building

--- ember-primitives/rollup.config.mjs
+++ ember-primitives/rollup.config.mjs
@@ -1,6 +1,8 @@
 import copy from "rollup-plugin-copy";
 import { babel } from "@rollup/plugin-babel";
 import { Addon } from "@embroider/addon-dev/rollup";
+import { execaCommand } from "execa";
+import { fixBadDeclarationOutput } from "fix-bad-declaration-output";
 
 const addon = new Addon({
   srcDir: "src",
@@ -26,5 +28,26 @@ export default {
         { src: "../LICENSE.md", dest: "." },
       ],
     }),
+    {
+      name: "Build Declarations",
+      closeBundle: async () => {
+        /**
+         * Generate the types (these include /// <reference types="ember-source/types"
+         * but our consumers may not be using those, or have a new enough ember-source that provides them.
+         */
+        console.log("Building types");
+        await execaCommand(`pnpm glint --declaration`, { stdio: "inherit" });
+        /**
+         * https://github.com/microsoft/TypeScript/issues/56571#
+         * README: https://github.com/NullVoxPopuli/fix-bad-declaration-output
+         */
+        console.log("Fixing types");
+        await fixBadDeclarationOutput("declarations/**/*.d.ts", [
+          ["TypeScript#56571", { types: "all" }],
+          "Glint#628",
+        ]);
+        console.log("⚠️ Dangerously (but neededly) fixed bad declaration output from typescript");
+      },
+    },
   ],
 };

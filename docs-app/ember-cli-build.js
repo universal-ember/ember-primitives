5cc552b7928e4b95c0075aaa078f6a4eb6b6b82f
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2024-06-27 14:40:33 -0400

    Get app watching working (#334)
    * Get dep syncing working
    
    * Remove options which do not exist

--- docs-app/ember-cli-build.js
+++ docs-app/ember-cli-build.js
@@ -1,10 +1,41 @@
 'use strict';
 
+const path = require('path');
+const fs = require('fs');
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = async function (defaults) {
+  const { readPackageUpSync } = await import('read-package-up');
+
   const app = new EmberApp(defaults, {
-    // Add options here
+    trees: {
+      app: (() => {
+        let sideWatch = require('@embroider/broccoli-side-watch');
+
+        let paths = ['ember-primitives'].map((libraryName) => {
+          let entry = require.resolve(libraryName);
+          let { packageJson, path: packageJsonPath } = readPackageUpSync({ cwd: entry });
+          let packagePath = path.dirname(packageJsonPath);
+
+          console.debug(
+            `Side-watching ${libraryName} from ${packagePath}, which started in ${entry}`
+          );
+
+          let toWatch = packageJson.files
+            .map((f) => path.join(packagePath, f))
+            .filter((p) => {
+              if (!fs.existsSync(p)) return false;
+              if (!fs.lstatSync(p).isDirectory()) return false;
+
+              return !p.endsWith('/src');
+            });
+
+          return toWatch;
+        });
+
+        return sideWatch('app', { watching: paths.flat() });
+      })(),
+    },
     'ember-cli-babel': {
       enableTypeScriptTransform: true,
       disableDecoratorTransforms: true,

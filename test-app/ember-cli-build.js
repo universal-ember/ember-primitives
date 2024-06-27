89e31220b3b6e7b6ee132feb1158408157c6aeab
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2024-06-27 14:34:28 -0400

    Get dep syncing working

--- test-app/ember-cli-build.js
+++ test-app/ember-cli-build.js
@@ -1,11 +1,40 @@
 'use strict';
 
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
+const path = require('path');
+const fs = require('fs');
 
-module.exports = function (defaults) {
+module.exports = async function (defaults) {
+  const { readPackageUpSync } = await import('read-package-up');
+
   let app = new EmberApp(defaults, {
-    autoImport: {
-      watchDependencies: ['ember-primitives'],
+    trees: {
+      app: (() => {
+        let sideWatch = require('@embroider/broccoli-side-watch');
+
+        let paths = ['ember-primitives'].map((libraryName) => {
+          let entry = require.resolve(libraryName);
+          let { packageJson, path: packageJsonPath } = readPackageUpSync({ cwd: entry });
+          let packagePath = path.dirname(packageJsonPath);
+
+          console.debug(
+            `Side-watching ${libraryName} from ${packagePath}, which started in ${entry}`
+          );
+
+          let toWatch = packageJson.files
+            .map((f) => path.join(packagePath, f))
+            .filter((p) => {
+              if (!fs.existsSync(p)) return false;
+              if (!fs.lstatSync(p).isDirectory()) return false;
+
+              return !p.endsWith('/src');
+            });
+
+          return toWatch;
+        });
+
+        return sideWatch('app', { watching: paths.flat() });
+      })(),
     },
     'ember-cli-babel': {
       enableTypeScriptTransform: true,

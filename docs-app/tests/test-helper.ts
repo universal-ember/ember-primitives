8e74efac00da0e1525a4360f0d49f9391b3ee366
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2024-11-12 16:40:10 -0500

    Define __pages__ earlier

--- docs-app/tests/test-helper.ts
+++ docs-app/tests/test-helper.ts
@@ -12,16 +12,6 @@ QUnit.config.urlConfig.push({
 });
 
 export async function start() {
-  let response = await fetch('/kolay-manifest/manifest.json');
-  let json = await response.json();
-  let pages = json.groups[0].list;
-
-  // The accessibility page deliberately
-  // has violations for demonstration
-  (window as any).__pages__ = pages.filter(
-    (page: { path: string }) => !page.path.includes('accessibility')
-  );
-
   setApplication(Application.create(config.APP));
 
   setup(QUnit.assert);

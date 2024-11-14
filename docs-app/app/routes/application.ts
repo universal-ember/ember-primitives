beee94d8ed8b75973194d3faad905459af9e9012
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2024-11-13 20:25:21 -0500

    Make tabster loading //

--- docs-app/app/routes/application.ts
+++ docs-app/app/routes/application.ts
@@ -10,10 +10,6 @@ import getWasm from 'shiki/wasm';
 import { APIDocs, ComponentSignature, ModifierSignature } from './api-docs';
 
 export default class Application extends Route {
-  async beforeModel() {
-    await setupTabster(this);
-  }
-
   async model() {
     const highlighter = await getHighlighterCore({
       themes: [import('shiki/themes/github-dark.mjs'), import('shiki/themes/github-light.mjs')],
@@ -33,48 +29,51 @@ export default class Application extends Route {
       loadWasm: getWasm,
     });
 
-    const manifest = await setupKolay(this, {
-      topLevelScope: {
-        Callout,
-        APIDocs,
-        ComponentSignature,
-        ModifierSignature,
-      },
-      resolve: {
-        // ember-primitives
-        'ember-primitives': import('ember-primitives'),
-        'ember-primitives/floating-ui': import('ember-primitives/floating-ui'),
-        'ember-primitives/color-scheme': import('ember-primitives/color-scheme'),
-        'ember-primitives/components/form': import('ember-primitives/components/form'),
+    const [manifest] = await Promise.all([
+      setupTabster(this),
+      setupKolay(this, {
+        topLevelScope: {
+          Callout,
+          APIDocs,
+          ComponentSignature,
+          ModifierSignature,
+        },
+        resolve: {
+          // ember-primitives
+          'ember-primitives': import('ember-primitives'),
+          'ember-primitives/floating-ui': import('ember-primitives/floating-ui'),
+          'ember-primitives/color-scheme': import('ember-primitives/color-scheme'),
+          'ember-primitives/components/form': import('ember-primitives/components/form'),
 
-        // community libraries
-        'ember-resources': import('ember-resources'),
-        'reactiveweb/remote-data': import('reactiveweb/remote-data'),
-        // @ts-expect-error - no types provided
-        'ember-focus-trap/modifiers/focus-trap': import('ember-focus-trap/modifiers/focus-trap'),
-        // @ts-expect-error - no types provided
-        'ember-focus-trap': import('ember-focus-trap'),
+          // community libraries
+          'ember-resources': import('ember-resources'),
+          'reactiveweb/remote-data': import('reactiveweb/remote-data'),
+          // @ts-expect-error - no types provided
+          'ember-focus-trap/modifiers/focus-trap': import('ember-focus-trap/modifiers/focus-trap'),
+          // @ts-expect-error - no types provided
+          'ember-focus-trap': import('ember-focus-trap'),
 
-        // utility
-        'lorem-ipsum': import('lorem-ipsum'),
-        'form-data-utils': import('form-data-utils'),
-        kolay: import('kolay'),
-      },
-      rehypePlugins: [
-        [
-          rehypeShikiFromHighlighter,
-          highlighter,
-          {
-            // Theme chosen by CSS variables in app/css/site/shiki.css
-            defaultColor: false,
-            themes: {
-              light: 'github-light',
-              dark: 'github-dark',
+          // utility
+          'lorem-ipsum': import('lorem-ipsum'),
+          'form-data-utils': import('form-data-utils'),
+          kolay: import('kolay'),
+        },
+        rehypePlugins: [
+          [
+            rehypeShikiFromHighlighter,
+            highlighter,
+            {
+              // Theme chosen by CSS variables in app/css/site/shiki.css
+              defaultColor: false,
+              themes: {
+                light: 'github-light',
+                dark: 'github-dark',
+              },
             },
-          },
+          ],
         ],
-      ],
-    });
+      }),
+    ]);
 
     return { manifest };
   }

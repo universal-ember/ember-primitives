e49c49452dead52483f6b68e0dffe29b1a85551e
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-11-23 23:07:51 -0500

    wip

--- docs-app/app/routes/api-docs.gts
+++ docs-app/app/routes/api-docs.gts
@@ -1,10 +1,21 @@
 import {
   APIDocs as KolayAPIDocs,
+  CommentQuery as KolayCommentQuery,
   ComponentSignature as KolayComponentSignature,
   ModifierSignature as KolayModifierSignature,
 } from 'kolay';
 
 import type { TOC } from '@ember/component/template-only';
+
+export function comment(name: string, declaration: string) {
+  return <template>
+    <KolayCommentQuery
+      @package="ember-primitives"
+      @module="declarations/{{declaration}}"
+      @name={{name}}
+    />
+  </template>;
+}
 
 export const APIDocs: TOC<{
   Args: { declaration: string; name: string };
--- docs-app/app/routes/application.ts
+++ docs-app/app/routes/application.ts
@@ -9,7 +9,7 @@ import { createOnigurumaEngine } from 'shiki/engine/oniguruma';
 import { Callout } from '@universal-ember/docs-support';
 
 import { Tabs } from '../components/tabs.gts';
-import { APIDocs, ComponentSignature, ModifierSignature } from './api-docs';
+import { APIDocs, ComponentSignature, ModifierSignature, comment } from './api-docs';
 
 export default class Application extends Route {
   async model() {
@@ -40,6 +40,7 @@ export default class Application extends Route {
           ComponentSignature,
           ModifierSignature,
           Tabs,
+          comment,
         },
         resolve: {
           // ember-primitives

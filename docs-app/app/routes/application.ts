014cce73779473323c130e464dd06492cbd43143
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-11-24 00:31:23 -0500

    Lint:fix

--- docs-app/app/routes/application.ts
+++ docs-app/app/routes/application.ts
@@ -9,7 +9,7 @@ import { createOnigurumaEngine } from 'shiki/engine/oniguruma';
 import { Callout } from '@universal-ember/docs-support';
 
 import { Tabs } from '../components/tabs.gts';
-import { APIDocs, ComponentSignature, ModifierSignature, comment } from './api-docs';
+import { APIDocs, comment, ComponentSignature, ModifierSignature } from './api-docs';
 
 export default class Application extends Route {
   async model() {

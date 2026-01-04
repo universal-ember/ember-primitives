617e23db86e4bd78fd755d5dd864f7bec660509d
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2026-01-04 09:44:38 -0500

    Unify

---
+++ packages/docs-support/eslint.config.mjs
@@ -0,0 +1,26 @@
+// eslint.config.js
+import { configs } from "@nullvoxpopuli/eslint-configs";
+
+const config = configs.ember(import.meta.dirname);
+
+export default [
+  ...config,
+  {
+    files: ["./src/components/toggle-group.gts"],
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+    },
+  },
+  {
+    files: ["**/*.{ts,gts}"],
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+    },
+  },
+  {
+    files: ["*.{js,cjs}"],
+    rules: {
+      "n/no-unsupported-features": "off",
+    },
+  },
+];

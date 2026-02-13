31e7bad7cd5b73bc09edaa811ea0a1ab78695c2d
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2026-02-13 18:02:12 -0500

    Fix babel

---
+++ test-app/babel.config.mjs
@@ -0,0 +1,52 @@
+import { fileURLToPath } from "node:url";
+import { buildMacros } from "@embroider/macros/babel";
+
+const macros = buildMacros();
+
+export default {
+  plugins: [
+    [
+      "@babel/plugin-transform-typescript",
+      {
+        allExtensions: true,
+        onlyRemoveTypeImports: true,
+        allowDeclareFields: true,
+      },
+    ],
+    [
+      "babel-plugin-ember-template-compilation",
+      {
+        compilerPath: "ember-source/dist/ember-template-compiler.js",
+        enableLegacyModules: [
+          "ember-cli-htmlbars",
+          "ember-cli-htmlbars-inline-precompile",
+          "htmlbars-inline-precompile",
+        ],
+        transforms: [...macros.templateMacros],
+      },
+    ],
+    [
+      "module:decorator-transforms",
+      {
+        runtime: {
+          // requires package.json#exports
+
+          import: fileURLToPath(import.meta.resolve("decorator-transforms/runtime-esm")),
+        },
+      },
+    ],
+    [
+      "@babel/plugin-transform-runtime",
+      {
+        absoluteRuntime: import.meta.dirname,
+        useESModules: true,
+        regenerator: false,
+      },
+    ],
+    ...macros.babelMacros,
+  ],
+
+  generatorOpts: {
+    compact: false,
+  },
+};

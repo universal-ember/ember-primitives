92ed8ef8a9f5bd221aac9aa58a9fb1164f6f67b0
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2026-05-15 10:59:35 -0400

    Fix internal types

--- ember-primitives/src/template-registry.ts
+++ ember-primitives/src/template-registry.ts
@@ -2,21 +2,21 @@
 // Add all your components, helpers and modifiers to the template registry here, so apps don't have to do this.
 // See https://typed-ember.gitbook.io/glint/using-glint/ember/authoring-addons
 
-import type { Accordion } from './components/accordion';
-import type { AccordionContent } from './components/accordion/content';
-import type { AccordionHeader } from './components/accordion/header';
-import type { AccordionItem } from './components/accordion/item';
-import type { AccordionTrigger } from './components/accordion/trigger';
-import type { Dialog } from './components/dialog';
-import type { ExternalLink } from './components/external-link';
-import type { Link } from './components/link';
-import type { Popover } from './components/popover';
-import type { Portal } from './components/portal';
-import type { PortalTargets } from './components/portal-targets';
-import type { Shadowed } from './components/shadowed';
-import type { Switch } from './components/switch';
-import type { Toggle } from './components/toggle';
-import type { service } from './helpers/service';
+import type { Accordion } from './components/accordion.gts';
+import type { AccordionContent } from './components/accordion/content.gts';
+import type { AccordionHeader } from './components/accordion/header.gts';
+import type { AccordionItem } from './components/accordion/item.gts';
+import type { AccordionTrigger } from './components/accordion/trigger.gts';
+import type { Dialog } from './components/dialog.gts';
+import type { ExternalLink } from './components/external-link.gts';
+import type { Link } from './components/link.gts';
+import type { Popover } from './components/popover.gts';
+import type { Portal } from './components/portal.gts';
+import type { PortalTargets } from './components/portal-targets.gts';
+import type { Shadowed } from './components/shadowed.gts';
+import type { Switch } from './components/switch.gts';
+import type { Toggle } from './components/toggle.gts';
+import type { service } from './helpers/service.ts';
 
 // import type MyComponent from './components/my-component';
 
